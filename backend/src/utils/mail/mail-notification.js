518bb8b7d65ad399e2c7db0b0ecf6710f2b4dca4
Ravi Patel <ravipateljigneshpatel137@gmail.com>
2024-12-09 17:32:14 +0530

    updating mail functionality

--- backend/src/utils/mail/mail-notification.js
+++ backend/src/utils/mail/mail-notification.js
@@ -5,18 +5,22 @@ dotenv.config();
 const sendMail = async(email, subject, content) => {
 
     const transporter = nodeMailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.MAIL_ID || 'tatvapca159@gmail.com',
             pass: process.env.MAIL_CRED || '',
         },
         tls: {
-            rejectUnauthorized: false,
+            ciphers: 'SSLv3', // Explicitly specify TLS configuration
         },
+        logger: true,  // Enable logs for debugging
+        debug: true,   // Show connection debug info
     });
 
     const mailOptions = {
-        from: 'FoodDeliveryApplication.com',
+        from: 'tatvapca159@gmail.com',
         to: email,
         subject: subject,
         text: content,
